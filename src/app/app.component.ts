85ad6273a9374c17c7f226ed6f3a3a5b5058e40d
no4paragon <46017825+no4paragon@users.noreply.github.com>
2020-11-17 10:08:23 -0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import * as algoliasearch from 'algoliasearch/lite';
 
 const searchClient = algoliasearch(
-  'O11NN7BWT9',
-  '92c882983b934a695b253b76de9ab457'
+  'xxxxxxxx',
+  'xxxxxxxxxxxxxxx'
 );
 
 @Component({
